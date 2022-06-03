cc62390d4b0f58770e4136171b33784ce6e14024
matt <madler@pdx.edu>
2022-06-03 13:28:29 -0700

    added js but cant see it in indexHtml

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+let item1 = document.querySelector("item-1");
+let divImg = document.createElement("div");
+let img = document.createElement("img");
+img.src = mtn.jpeg;
+img.setAttribute("alt","Mountain");
+img.style.borderRadius = "90%";
+img.style.margin = "10%";
+item1.append(divImg);
+divImg.append(img);
+
 
