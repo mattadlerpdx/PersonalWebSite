918220d782e130422c1e9dd1192dc2785f090e52
matt <madler@pdx.edu>
2022-06-05 14:48:51 -0700

    spaced elements correctly

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ let item1 = document.querySelector(".item-1");
 let divImg = document.createElement("div");
 let img = document.createElement("img");
 img.src = 'mtn.jpeg';
+
 img.setAttribute("alt","Mountain");
-img.style.borderRadius = "90%";
+img.style.borderRadius = "2.5%";
 img.style.margin = "10%";
 item1.append(divImg);
 divImg.append(img);
