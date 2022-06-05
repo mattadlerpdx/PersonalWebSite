f28708dc178e78d21fa306459ffe41e6bfc6eb45
matt <madler@pdx.edu>
2022-06-05 16:09:45 -0700

    done

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let divImg = document.createElement("div");
 let img = document.createElement("img");
 img.src = 'mtn.jpeg';
 
-img.setAttribute("alt","Mountain");
+img.setAttribute("alt", "Mountain");
 img.style.borderRadius = "2.5%";
-img.style.margin = "10%";
+img.style.margin = "5%";
 item1.append(divImg);
 divImg.append(img);
 
