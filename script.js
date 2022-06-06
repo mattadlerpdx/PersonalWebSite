4037f6e2465b29cd5ed5af0d4d4a21918b72043a
matt <madler@pdx.edu>
2022-06-05 20:26:04 -0700

    added form controls

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+let form = document.querySelector("form");
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    checkUser();
+    checkEmail();
+    checkMessage();
+    event.preventDefault(event);
+}
+function checkUser() {
+    let userName = form.elements.username.value;
+    if (userName !== "") {
+        console.log("Username: ", userName);
+        return true;
+    } else {
+        console.log("no submission");
+        return false;
+    }
+}
+function checkEmail() {
+    let email = form.elements.email.value;
+    if (email !== "") {
+        console.log("Email: ", email);
+        return true;
+    } else {
+        console.log("no submission");
+        return false;
+    }
+}
+function checkMessage() {
+    let message = form.elements.message.value;
+    if (message !== "") {
+        console.log("message: ", message);
+        return true;
+    } else {
+        console.log("no submission");
+        return false;
+    }
+}
+
+
+
 let item1 = document.querySelector(".item-1");
 let divImg = document.createElement("div");
 let img = document.createElement("img");
@@ -8,5 +50,3 @@ img.style.borderRadius = "2.5%";
 img.style.margin = "5%";
 item1.append(divImg);
 divImg.append(img);
-
-
